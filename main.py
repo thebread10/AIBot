6b0038192555596c5b132578b0ed8472b9022e71
Sahil Raut <78951457+enderman2003@users.noreply.github.com>
2023-01-19 23:31:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -42,9 +42,10 @@ def check_all_messages(message):
     # Responses -------------------------------------------------------------------------------------------------------
     response('Hello!', dataset.hello, single_response=True)
     response('See you!', dataset.goodbye, single_response=True)
-    response('I\'m doing fine, and you?', dataset.how_asking, ['how', 'hru'])
+    response('I\'m doing fine, and you?', dataset.how_asking, required_words=['how', 'hru'])
+    response('I\'m a lifeless human with only humanoid brain which is created artificially - A bot', dataset.who_asking, single_response=True)
     response('You\'re welcome!', dataset.thanks, single_response=True)
-    
+    response('Aww! Thanks', dataset.grateful, single_response=True)
     # Longer responses
     response(long.R_ADVICE, ['give', 'advice'], required_words=['advice'])
     response(long.R_EATING, ['what', 'you', 'eat'], required_words=['you', 'eat'])
