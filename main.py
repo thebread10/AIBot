6d830d280657a6bc00e39287af97a248c54562c1
Sahil Raut <78951457+enderman2003@users.noreply.github.com>
2023-01-19 23:51:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -47,6 +47,9 @@ def check_all_messages(message):
     response('I\'m a lifeless human-type with only humanoid brain which is created artificially - A bot', dataset.who_asking, single_response=True)
     response('You\'re welcome!', dataset.thanks, single_response=True)
     response('Aww! Thanks', dataset.grateful, single_response=True)
+    response('Glad to hear. So what did you do today?', dataset.reply, single_response=True)
+    response('Oh! interesting ?', dataset.routine, single_response=True)
+    
     # Longer responses
     response(long.R_ADVICE, ['give', 'advice'], required_words=['advice'])
     response(long.R_EATING, ['what', 'you', 'eat'], required_words=['you', 'eat'])
