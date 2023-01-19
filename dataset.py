ff056acf95368291e4d1a28718cd237219cbc31f
Sahil Raut <78951457+enderman2003@users.noreply.github.com>
2023-01-19 20:37:15 +0530

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -1,3 +1,4 @@
 hello = ['hello', 'hi', 'hey', 'sup', 'heyo']
 thanks = ['Thanks', 'you', 'Thank']
 how_asking = ['how', 'are', 'you', 'hru']
+goodbye = ['goodbye', 'bye', 'see you', 'cya', 'farewell']
