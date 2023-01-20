76a3c016c97b4b681d35a02724ada300e1eecab8
Sahil Raut <78951457+enderman2003@users.noreply.github.com>
2023-01-20 09:41:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,10 +7,10 @@ from discord.ext import commands
 
 bot = commands.Bot(command_prefix = "-", intents = discord.Intents.all())
 
-def message_probability(user_message, recognised_words, single_response=False, required_words=[], negative_response=False, reply=False):
+def message_probability(user_message, recognised_words, single_response=False, required_words=[], negative_response=False, reply=[]):
     message_certainty = 0
     has_required_words = True
-
+    is_reply = False
     # Counts how many words are present in each predefined message
     for word in user_message:
         if word in recognised_words:
@@ -18,7 +18,12 @@ def message_probability(user_message, recognised_words, single_response=False, r
 
     # Calculates the percent of recognised words in a user message
     percentage = float(message_certainty) / float(len(recognised_words))
-
+    
+    for word in reply:
+        if word in user_message:
+            is_reply = True
+            break
+    
     # Checks that the required words are in the string
     for word in required_words:
         if word not in user_message:
@@ -28,7 +33,7 @@ def message_probability(user_message, recognised_words, single_response=False, r
     # Must either have the required words, or be a single response
     if has_required_words or single_response:
         return int(percentage * 100)
-    elif reply:
+    elif is_reply:
         return -1
     else:
         return 0
@@ -38,7 +43,7 @@ def check_all_messages(message):
     highest_prob_list = {}
 
     # Simplifies response creation / adds it to the dict
-    def response(bot_response, list_of_words, single_response=False, required_words=[], negative_response=False, reply=False):
+    def response(bot_response, list_of_words, single_response=False, required_words=[], negative_response=False, reply=[]):
         nonlocal highest_prob_list
         highest_prob_list[bot_response] = message_probability(message, list_of_words, single_response, required_words, negative_response, reply)
 
@@ -54,7 +59,7 @@ def check_all_messages(message):
     response('Oh! interesting ?', dataset.routine, single_response=True)
     response('Talking with you, ig? wbu', dataset.ask, required_words=["what", "doing"])
     response('Talking with you, ig? wbu', dataset.ask, required_words=["what", "upto"])
-    response('Oh what is it, is it interesting?', dataset.tasks, single_response=True, reply=True)
+    response('Oh what is it, is it interesting?', dataset.tasks, single_response=True, reply=['yes', 'no'])
 
     # Longer responses
     response(long.R_ADVICE, ['give', 'advice'], required_words=['advice'])
