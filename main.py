3cbf2ac22e7ae5e0c472f7e5af80cfc5325bb54c
Sahil Raut <78951457+enderman2003@users.noreply.github.com>
2023-01-22 11:33:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,9 +9,7 @@ API_URL = "https://api-inference.huggingface.co/models/deepparag/Aeona"
 headers = {"Authorization": f"Bearer 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"}
 
 def query(payload):
-    data = json.dumps(payload)
-    response = requests.request("POST", API_URL, headers=headers, data=data)
-    print(json.loads(response.content.decode("utf-8")))
+    response = requests.request("POST", API_URL, headers=headers, json=payload)
     return json.loads(response.content.decode("utf-8").generated_text)
 
 @bot.event
@@ -26,7 +24,12 @@ async def on_message(message):
     if message.author == bot.user:
         return
     if message.content != "":
-        await message.channel.send(query(message.content))
+        await message.channel.send(query({
+	"inputs": {
+		"past_user_inputs": message.content,
+		"generated_responses": ["It's Die Hard for sure."],
+		"text": message.content
+	})
     await bot.process_commands(message)
 
 bot.run("MTA2NTY1MDMzMDU5Mjg3ODcyNA.GTWMfV.zxlQ7zPKZCLnDF4qIsgzjsvF74jZJmq1bb3lkA")
