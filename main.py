e98116ea0fb8a65e2231ccd67ca2cfa83a6bdf87
Sahil Raut <78951457+enderman2003@users.noreply.github.com>
2023-01-22 16:32:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,13 +8,23 @@ bot = commands.Bot(command_prefix = "L?", intents = discord.Intents.all())
 
 API_URL = f"https://api-inference.huggingface.co/models/{os.getenv('MODEL_ID')}"
 headers = {"Authorization": f"Bearer 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"}
+
 data = {
     'guild_id': [],
     'channel_id': []
 }
+
+message_payload = {
+    "inputs": {
+        "past_user_inputs": [],
+        "generated_responses": [],
+        "text": message.content
+    }
+}
+
 def query(payload):
     response = requests.request("POST", API_URL, headers=headers, json=payload)
-    return response.json()['generated_text']
+    return response.json()
 
 @bot.command()
 async def export_data(ctx):
@@ -56,13 +66,11 @@ async def on_message(message):
 
     if isPresent == True:
         channel = bot.get_channel(data['channel_id'][data['guild_id'].index(message.guild.id)])
-        await channel.send(query({
-	    "inputs": {
-		"past_user_inputs": [],
-		"generated_responses": [],
-		"text": message.content
-            }
-        }))
+        message_payload['inputs']['text'] = message.content
+        message_payload['inputs']['past_user_inputs'].append(message.content)
+        res = query(message_payload)
+        message_payload['inputs']['generated_responses'].append(res)
+        await channel.send(res)
     else:
         await message.channel.send("No channels set")
     await bot.process_commands(message)
