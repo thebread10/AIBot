44365d7114a3c8c4c808acd235e97be8159562e4
Sahil Raut <78951457+enderman2003@users.noreply.github.com>
2023-01-24 23:04:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -18,6 +18,9 @@ data = {
     'channel_id': []
 }
 
+past_msg = []
+responses = []
+
 @bot.command()
 async def export_data(ctx):
      print(data)
@@ -62,13 +65,13 @@ async def on_message(message):
 
     if isPresent == True:
         channel = bot.get_channel(data['channel_id'][data['guild_id'].index(message.guild.id)])
-        data_msg = { "inputs": { "past_user_inputs": [], "generated_responses": [], "text": message.content } }
-        data_msg["inputs"]["past_user_inputs"].append(message.content)
-        if len(data_msg["inputs"]["past_user_inputs"]) == 3:
-            data_msg["inputs"]["generated_responses"].append(res["generated_text"])["inputs"]["past_user_inputs"].clear()
-            data_msg["inputs"]["generated_responses"].clear()
+        data_msg = { "inputs": { "past_user_inputs": past_msg, "generated_responses": responses, "text": message.content } }
+        past_msg.append(message.content)
+        if len(past_msg) == 3:
+            past_msg.clear()
+            responses.clear()
         res = query(data_msg)  
-        data_msg["inputs"]["generated_responses"].append(res["generated_text"])
+        responses.append(res["generated_text"])
         await channel.send(res["generated_text"])
     else:
         await message.channel.send("No channels set")
